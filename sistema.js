fa0d788231dddfd80f8f29818a8072e00cab1edd
JoaoBessestil <joaobessestil@gmail.com>
2021-09-02 21:08:49 -0300

    .

---
+++ sistema.js
@@ -0,0 +1,65 @@
+let numeroDeAlunos = 10
+
+for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+
+    if (contador === 0) {
+        console.log("O número atual é ZERO")
+    } else if (contador % 2 == 0) {
+        console.log("O número " + contador + " é PAR")
+    } else {
+        console.log("O número " + contador + " é ÍMPAR")
+    }
+}
+
+let numeroDeAlunos = 10
+let contador = 0
+
+while (contador <= 10) {
+    if (contador === 0) {
+        console.log("O número atual é ZERO")
+    } else if (contador % 2 == 0) {
+        console.log("O número " + contador + " é PAR")
+    } else {
+        console.log("O número " + contador + " é ÍMPAR")
+
+    }
+    contador++
+}
+
+let numeroDeAlunos = 10
+
+let contador = 0
+
+do {
+    if (contador === 0) {
+        console.log("O número atual é ZERO")
+    } else if (contador % 2 == 0) {
+        console.log("O número " + contador + " é PAR")
+    } else {
+        console.log("O número " + contador + " é ÍMPAR")
+
+    }
+    contador++
+}
+
+while (contador <= 10)
+
+
+ //OPERADORES LÓGICOS 
+
+//AND (E)
+//true && true -> true
+//true && false -> false
+//false && true -> false
+//false && false -> false
+
+//OR (OU)
+//true || true -> true
+//true || false -> true
+//false || true -> true
+//false || false -> false
+
+//NOT (NÂO)
+//!false -> true
+//!true -> false
+
